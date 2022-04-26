6a80c629e6cbb9f9beb0e80b5f38aeadd2cc2f4c
Charles Hall <webdevcharles@gmail.com>
2022-04-26 17:58:12 -0400

    Add project files.

---
+++ GenesisAddressBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GenesisAddressBook.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GenesisAddressBook.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Contact> Contacts { get; set; } = default!;
+        public virtual DbSet<Category> Categories { get; set; } = default!;
+
+
+
+    }
+}
---
+++ GenesisAddressBook/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GenesisAddressBook.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string? AppUserId { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string? Name { get; set; }
+
+        // TO-DO: Add Virtuals //
+
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+
+    }
+}
